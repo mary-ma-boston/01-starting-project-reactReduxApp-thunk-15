427c125d6daba5bc5cd03eb74adb8eba5e3b9b3b
mayue <mayue@mayues-MacBook-Pro.local>
2023-08-04 14:07:44 -0400

    orderbook

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useSelector } from 'react-redux';
+
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state=>state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+
+  }
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
